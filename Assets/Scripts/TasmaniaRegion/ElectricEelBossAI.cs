12bd0f643959859c8a681bac8ccfc92b458561a6
speedy81523 <tylerjamestan@gmail.com>
2026-02-24 14:21:16 +0800

    boss stuff still not working

--- Assets/Scripts/TasmaniaRegion/ElectricEelBossAI.cs
+++ Assets/Scripts/TasmaniaRegion/ElectricEelBossAI.cs
@@ -3,6 +3,20 @@ using UnityEngine;
 using UnityEngine.AI;
 public class ElectricEelBossAI : BossAI
 {
+
+
+    private enum BossState
+    {
+        Patrol,
+        Chase,
+        Attack,
+        Retreat
+    }
+
+    private BossState currentState = BossState.Patrol;
+
+
+
     public Transform[] patroPoints;
     private NavMeshAgent agent;
     private int currentLoc;
@@ -33,96 +47,186 @@ public class ElectricEelBossAI : BossAI
 
     //testing cause ai keeps tracking and returning to player while retreating
     //works then dosen't work so very confusing
+
     private float ignorePlayerUntil = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
         agent = GetComponent<NavMeshAgent>();
         if (patroPoints.Length > 0)
         {
             agent.SetDestination(patroPoints[currentLoc].position); //set where its going to pathfind too
         }
     }
-
+    //change everything to state switching HHHHH
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
 
 
+        base.Update(); //call from base class to update health bar and all t hat
+      
+
+        switch (currentState)
+        {
+            case BossState.Patrol:
+                HandlePatrol();
+                break;
+
+            case BossState.Chase:
+                HandleChase();
+                break;
+
+            case BossState.Attack:
+                break; 
+
+            case BossState.Retreat:
+                break; 
+        }
+
+        //Debug.Log($"shipl: {shipl}, isRetreating: {isRetreating}, agent: {agent}"); //testing
+        //EncounterPlayer();
+        ////if (!isRetreating && Time.time > ignorePlayerUntil)
+        ////{
+        ////    EncounterPlayer();
+        ////}
+        //if (isRetreating)
+        //    return;
+
+        ////if (Time.time > ignorePlayerUntil) //hope
+        ////{
+        ////    EncounterPlayer();
+        ////}
+        ////else
+        ////{
+        ////    shipl = null;
+        ////}
         
-        if (isRetreating)
-            return;
-
-        if (Time.time > ignorePlayerUntil) //hope
-        {
-            EncounterPlayer();
-        }
-        else
-        {
-            shipl = null;
-        }
 
 
-
-        if (shipl != null)
-        {
+        //if (shipl != null)
+        //{
           
-            float distance = Vector3.Distance(transform.position, shipl.position);
+        //    float distance = Vector3.Distance(transform.position, shipl.position);
 
-            if (distance > attackRange)
-            {
-                wasChasing = true;
+        //    if (distance > attackRange)
+        //    {
+        //        agent.isStopped = false;
                
-                agent.SetDestination(shipl.position);
-            }
-            else
-            {
-                //attack
-                wasChasing = true;
+        //        agent.SetDestination(shipl.position);
+        //    }
+        //    else
+        //    {
+        //        ////attack
+        //        //wasChasing = true;
 
-                if (Time.time >= lastAttackTime + attackCooldown)
-                {
-                    StartCoroutine(Attack());
-                }
-            }
-        }
-        else
-        {
-            //if (!agent.pathPending && agent.remainingDistance < 0.5)
-            //{
-            //    GoToNextPoint(); //subject to change
-            //}
-            if (wasChasing)
-            {
-                wasChasing = false;
-                GoToNextPoint(); //restart patrol after chasing
-            }
+        //        //if (Time.time >= lastAttackTime + attackCooldown)
+        //        //{
+        //        //    StartCoroutine(Attack());
+        //        //}
+        //        if (!isAttacking && Time.time >= lastAttackTime + attackCooldown)
+        //        {
+        //            StartCoroutine(Attack());
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    //if (!agent.pathPending && agent.remainingDistance < 0.5)
+        //    //{
+        //    //    GoToNextPoint(); //subject to change
+        //    //}
+        //    if (wasChasing)
+        //    {
+        //        wasChasing = false;
+        //        GoToNextPoint(); //restart patrol after chasing
+        //    }
 
-            // Normal patrol
-            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
-            {
-                GoToNextPoint();
-            }
-        }
+        //    // Normal patrol
+        //    if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        //    {
+        //        GoToNextPoint();
+        //    }
+        //}
        
        
 
 
     }
+
+    void HandlePatrol()
+    {
+        EncounterPlayer();
+
+        if (shipl != null)
+        {
+          
+            return;
+        }
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void HandleChase()
+    {
+
+        //EncounterPlayer();
+        if (shipl == null)
+        {
+            currentState = BossState.Patrol;
+            return;
+        }
+        agent.isStopped = false;
+        float distance = Vector3.Distance(transform.position, shipl.position);
+
+        if (distance > attackRange)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(shipl.position);
+        }
+        else
+        {
+            if (Time.time >= lastAttackTime + attackCooldown)
+            {
+                currentState = BossState.Attack;
+                StartCoroutine(Attack());
+            }
+        }
+
+
+
+    }
+
     public void EncounterPlayer()
     {
+
+
+        if (Time.time < ignorePlayerUntil) //testing
+            return;
+
+        if (currentState == BossState.Retreat || isAttacking) //testing
+            return;
         Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius, shipLayer);
         if (hits.Length > 0)
         {
             shipl = hits[0].transform;
-         
+            wasChasing = true;
+            currentState = BossState.Chase;
+            Debug.Log("Player detected: " + shipl.name);
         }
         else
         {
-            shipl = null; //if move out then continuing patrolling
-          
-
+            //shipl = null; //if move out then continuing patrolling
+            //clear when far
+            if (shipl != null && Vector3.Distance(transform.position, shipl.position) > detectionRadius + 2f)
+            {
+                shipl = null;
+                currentState = BossState.Patrol;
+            }
         }
 
         //testing for now
@@ -142,7 +246,7 @@ public class ElectricEelBossAI : BossAI
     {
         isAttacking = true;
         lastAttackTime = Time.time;
-
+       // agent.isStopped = true;
         int boltCount = Random.Range(2, 6); //change later see how
 
         for (int i = 0; i < boltCount; i++)
@@ -152,11 +256,18 @@ public class ElectricEelBossAI : BossAI
         }
 
         StartCoroutine(Retreat());
+        currentState = BossState.Retreat;
         isAttacking = false;
     }
 
     IEnumerator Retreat()
     {
+
+        if (shipl == null)
+        {
+            currentState = BossState.Patrol;
+            yield break;
+        }
         isRetreating = true;
         agent.isStopped = false;
 
@@ -178,11 +289,12 @@ public class ElectricEelBossAI : BossAI
         }
         yield return new WaitForSeconds(1f); //wait bit more for now lol
         isRetreating = false;
-
+        isAttacking = false;
         ignorePlayerUntil = Time.time + 3f; //fix should pls work
         shipl = null;
+        currentState = BossState.Patrol;
         // Resume patrol cleanly
-        GoToNextPoint();
+        //GoToNextPoint();
     }
     public void SpawnLighting()
     {
